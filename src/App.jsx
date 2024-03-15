9da504a5ec8e2a391048db65b6a4133eb2c0ab7f
Mohit76655 <mohit76655@gmail.com>
2024-03-15 23:28:10 +0530

    modif

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,26 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Hero />
-        <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Benefits />
+          <Collaboration />
+          <Services />
+          <Pricing />
+          <Roadmap />
+        </Routes>
         <Footer />
       </div>
 
       <ButtonGradient />
-    </>
+    </BrowserRouter>
   );
 };
 
--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -8,7 +8,7 @@ const Generating = ({ className }) => {
       } text-base`}
     >
       <img className="w-5 h-5 mr-4" src={loading} alt="Loading" />
-      AI is generating
+      <span className=" text-slate-300">Eg. Search</span>
     </div>
   );
 };
