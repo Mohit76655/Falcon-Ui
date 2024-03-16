1d132a29f369c8d28aef6ed507e2cb14d93e7525
Piyush Tak <piyushtak333@gmail.com>
2024-03-16 18:41:11 +0530

    services added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,9 @@
 import Section from "./Section";
 import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
-import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import { brainwaveServicesIcons } from "../constants";
 import {
-  PhotoChatMessage,
+
   Gradient,
   VideoBar,
   VideoChatMessage,
@@ -16,8 +16,8 @@ const Services = () => {
     <Section id="how-to-use">
       <div className="container">
         <Heading
-          title="Generative AI made for creators."
-          text="Brainwave unlocks the potential of AI-powered applications"
+          title="Samples Ui Design made for creators."
+          text="Falcon unlocks the potential of Developers"
         />
 
         <div className="relative">
@@ -31,25 +31,6 @@ const Services = () => {
                 src={service1}
               />
             </div>
-
-            <div className="relative z-1 max-w-[17rem] ml-auto">
-              <h4 className="h4 mb-4">Smartest AI</h4>
-              <p className="body-2 mb-[3rem] text-n-3">
-                Brainwave unlocks the potential of AI-powered applications
-              </p>
-              <ul className="body-2">
-                {brainwaveServices.map((item, index) => (
-                  <li
-                    key={index}
-                    className="flex items-start py-4 border-t border-n-6"
-                  >
-                    <img width={24} height={24} src={check} />
-                    <p className="ml-4">{item}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-
             <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
 
@@ -65,15 +46,13 @@ const Services = () => {
                 />
               </div>
 
-              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
-                <h4 className="h4 mb-4">Photo editing</h4>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15 opacity-0 hover:opacity-[1] cursor-pointer">
+              <h4 className="h4 mb-4 ">Photo editing</h4>
                 <p className="body-2 mb-[3rem] text-n-3">
                   Automatically enhance your photos using our AI app&apos;s
                   photo editing feature. Try it now!
                 </p>
               </div>
-
-              <PhotoChatMessage />
             </div>
 
             <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
@@ -116,8 +95,6 @@ const Services = () => {
                   height={400}
                   alt="Scary robot"
                 />
-
-                <VideoChatMessage />
                 <VideoBar />
               </div>
             </div>
